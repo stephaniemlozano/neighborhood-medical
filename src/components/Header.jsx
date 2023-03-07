27ee0c54d61505df4ccde30c5291aff0af0daee4
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2023-03-06 21:09:57 -0500

    update img to local folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import { Container, Nav, Navbar} from 'react-bootstrap';
-
+import logo1 from '../assets/images/logoTransparentBg.png'
 const Header = () => {
     return (
 
       
         <Navbar bg="light" variant={"light"} expand="lg">         
-        <img className="logo-image-1" src="https://rafael-reyes-bucket.s3.amazonaws.com/logoTransparentBg.png" alt="logo-image1"/>     
-      
+        {/* <img className="logo-image-1" src="https://rafael-reyes-bucket.s3.amazonaws.com/logoTransparentBg.png" alt="logo-image1"/>      */}
+        <img className="logo-image-1" src={logo1} alt="logo-image1"/>  
         <Container >
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
